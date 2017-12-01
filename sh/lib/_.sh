9dd175779f2c45eec90aa6126733818bf5763cbf
Zach Gianos <zach.gianos@gmail.com>
2017-11-30 16:27:22 -0800

    Shell: _.sh comments and refactoring
    _.sh:
    - adds function comment to realdir
    - removes gi_import in favor of lib function gi

--- sh/lib/_.sh
+++ sh/lib/_.sh
@@ -1,22 +1,62 @@
 #!/usr/bin/env bash
 
 
-realdir () {
-  local file=$1
-  local dir
+###############################################################################
+# Prints the directory of the canonical absolute path of `path`.
+#
+# All relative path identifiers (`.` and `..`) and symbolic links are resolved.
+# If `path` is a symbolic link, the result is the directory of the file after
+# following the link, recursively if necessary.
+#
+# Globals:
+#   None
+# Options:
+#   None
+# Arguments:
+#   path - the path to resolve
+# Returns:
+#   None
+# See:
+#   absdir
+###############################################################################
+realdir() {
+  local path="${1:-}"
+  local dir="$( cd -P "$( dirname "${path}" )" && pwd -P )"
   # resolve until no longer a symlink
-  while [ -h "${file}" ]; do
-    dir="$(cd -P "$(dirname "$file")" && pwd)"
-    file="$(readlink "$file")"
-    # if relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $file != /* ]] && file="$dir/$file"
+  while [ -h "${path}" ]; do
+    path="$(readlink "${path}")"
+    # if 'path' is a relative path, resolve it relative to 'dir'
+    # the directory containing the link
+    [[ "${path}" != /* ]] && path="${dir}/${path}"
+    dir="$( cd -P "$( dirname "${path}" )" && pwd -P )"
   done
-  dir="$(cd -P "$(dirname "$file")" && pwd)"
-  echo "$dir"
+  echo "${dir}"
 }
+
 
 GI_SHLIB_DIR="$(realdir "${BASH_SOURCE[0]}")"
 
-gi_import () {
-  source "${GI_SHLIB_DIR}/$1.sh"
+gi() {
+  local action="${1:-}"; shift
+  case "${action}" in
+    import)
+      for lib in "$@"; do
+        local libpath="${GI_SHLIB_DIR}/${lib}.sh"
+        if [ ! -f "${libpath}" ]; then
+          echo "gi: unknown library '${lib}'" >&2
+          return 1
+        fi
+        source "${libpath}"
+      done
+      ;;
+    *)
+      local script="${GI_SHLIB_DIR}/${action}.sh"
+      if [ ! -f "${script}" ]; then
+        echo "gi: unknown action '${action}'" >&2
+        return 1
+      fi
+      ${SHELL} "${script}" "$@"
+      return $?
+      ;;
+  esac
 }
