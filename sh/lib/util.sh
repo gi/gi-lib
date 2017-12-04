e4a647c369f79c70ee2082e10c6a300e1f931349
Zach Gianos <zach.gianos@gmail.com>
2017-12-04 13:48:21 -0800

    Shell: updates cdup to call cd once.

--- sh/lib/util.sh
+++ sh/lib/util.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 
-cdup () {
-  local depth=$1
-  [ -z "${depth}" ] && depth=1
+cdup() {
+  local depth="${1:-1}"
+  local dir="$( pwd )"
   while ((depth-- > 0)); do
-    cd ..
+    dir="$( dirname "${dir}" )"
   done
+  cd "${dir}"
 }
